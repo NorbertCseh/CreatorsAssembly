e122366b19606a781b98f3740bf992c6c98b5fd3
NorbertCseh <cseh.norbert94@gmail.com>
2019-05-09 15:30:49 +0200

    CSP

---
+++ client/build/precache-manifest.7e0d0cbd391654cdef18d8bd4641dcd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c45543f136baabd55405",
+    "url": "/static/js/main.4b20d8a3.chunk.js"
+  },
+  {
+    "revision": "28b0023a855606c6bdee",
+    "url": "/static/js/2.00e31144.chunk.js"
+  },
+  {
+    "revision": "c45543f136baabd55405",
+    "url": "/static/css/main.99be1ae3.chunk.css"
+  },
+  {
+    "revision": "330ce9d90cea29a80d6ce6b9ac1b3a50",
+    "url": "/index.html"
+  }
+];
